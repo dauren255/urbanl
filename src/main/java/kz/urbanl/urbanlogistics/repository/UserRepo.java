c84dfdf5afe583e032688da43779ad7f57e3e3fe
b.dauren <figa99.dt@gmail.com>
2021-05-11 22:19:56 +0600

    initial commit

--- src/main/java/kz/urbanl/urbanlogistics/repository/UserRepo.java
+++ src/main/java/kz/urbanl/urbanlogistics/repository/UserRepo.java
@@ -1,10 +1,16 @@
 package kz.urbanl.urbanlogistics.repository;
 
+import kz.urbanl.urbanlogistics.model.Status;
 import kz.urbanl.urbanlogistics.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsernameIgnoreCase(String name);
+    List<User> findAllByStatus(Status status);
+    User findUserById(Long id);
+    List<User> findAllByAccountNonExpired(boolean accountNonExpired);
 }
